04f009a5fd3956e7e06c8db58f589ba2198621bc
Md Rashedul Islam <santorasu09@gmail.com>
2025-05-21 21:04:51 +0600

    This is google maps by API

---
+++ android/app/src/main/kotlin/com/geomaps/google_maps/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.geomaps.google_maps
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Home")),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(23.880815840607823, 90.32470045429187),
+        ),
+      ),
+    );
+  }
+}
